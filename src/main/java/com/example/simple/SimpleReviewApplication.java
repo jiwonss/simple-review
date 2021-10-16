fcedf72385092ac5b3ff4f609ab9404c328282ef
jiwonss <sjs3326@naver.com>
2021-10-16 23:56:52 +0900

    프로젝트명 변경

---
+++ src/main/java/com/example/simple/SimpleReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleReviewApplication.class, args);
+	}
+
+}
