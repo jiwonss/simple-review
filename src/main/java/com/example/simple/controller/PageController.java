cd3b5b303a5b52d92d08aca74568abcedcb115e5
jiwonss <sjs3326@naver.com>
2021-10-21 17:51:12 +0900

    main page 생성

---
+++ src/main/java/com/example/simple/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.example.simple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/pages")
+public class PageController {
+
+    @GetMapping("/main")
+    public ModelAndView main(){
+        return new ModelAndView("main");
+    }
+
+    @GetMapping("/join")
+    public ModelAndView join(){
+        return new ModelAndView("join");
+    }
+}
