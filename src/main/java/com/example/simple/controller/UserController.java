e0019279a32cae84c50d0fd89be203dfdf31634d
jiwonss <sjs3326@naver.com>
2021-11-09 21:12:29 +0900

    API

--- src/main/java/com/example/simple/controller/UserController.java
+++ src/main/java/com/example/simple/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.example.simple.controller;
 
 import com.example.simple.user.dto.UserDto;
-import com.example.simple.user.entity.UserEntity;
-import com.example.simple.user.repository.UserRepository;
 import com.example.simple.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,14 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class UserController {
 
     private final UserService userService;
-    private final UserRepository userRepository;
 
     // 회원가입
     @PostMapping(value = "/user")
---
+++ src/test/java/com/example/simple/naver/NaverClientTest.java
@@ -0,0 +1,24 @@
+package com.example.simple.naver;
+
+import com.example.simple.naver.dto.SearchLocalReq;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NaverClientTest {
+
+    @Autowired
+    private NaverClient naverClient;
+
+    @Test
+    public void searchTest() {
+        var search = new SearchLocalReq();
+        search.setQuery("갈비집");
+
+        var result = naverClient.searchLocal(search);
+        System.out.println(result);
+    }
+}
