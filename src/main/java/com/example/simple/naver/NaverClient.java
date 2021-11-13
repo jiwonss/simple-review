9d8ad875c9d0a96431c558af8723a76f4e8cf34b
jiwonss <sjs3326@naver.com>
2021-11-13 23:54:46 +0900

    이미지 API 연동

--- src/main/java/com/example/simple/naver/NaverClient.java
+++ src/main/java/com/example/simple/naver/NaverClient.java
@@ -1,5 +1,7 @@
 package com.example.simple.naver;
 
+import com.example.simple.naver.dto.SearchImageReq;
+import com.example.simple.naver.dto.SearchImageRes;
 import com.example.simple.naver.dto.SearchLocalReq;
 import com.example.simple.naver.dto.SearchLocalRes;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,6 +26,9 @@ public class NaverClient {
     @Value("${naver.url.search.local}")
     private String naverLocalSearchUrl;
 
+    @Value("${naver.url.search.image}")
+    private String naverImageSearchUrl;
+
     public SearchLocalRes searchLocal(SearchLocalReq searchLocalReq) {
         var uri = UriComponentsBuilder.fromUriString(naverLocalSearchUrl)
                 .queryParams(searchLocalReq.toMultiValueMap())
@@ -38,6 +43,31 @@ public class NaverClient {
 
         var httpEntity = new HttpEntity<>(headers);
         var responseType = new ParameterizedTypeReference<SearchLocalRes>(){};
+
+        var responseEntity = new RestTemplate().exchange(
+                uri,
+                HttpMethod.GET,
+                httpEntity,
+                responseType
+        );
+
+        return responseEntity.getBody();
+    }
+
+    public SearchImageRes searchImage(SearchImageReq searchImageReq) {
+        var uri = UriComponentsBuilder.fromUriString(naverImageSearchUrl)
+                .queryParams(searchImageReq.toMultiValueMap())
+                .build()
+                .encode()
+                .toUri();
+
+        var headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", naverClientId);
+        headers.set("X-Naver-Client-Secret", naverClientSecret);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        var httpEntity = new HttpEntity<>(headers);
+        var responseType = new ParameterizedTypeReference<SearchImageRes>(){};
 
         var responseEntity = new RestTemplate().exchange(
                 uri,
--- src/test/java/com/example/simple/naver/NaverClientTest.java
+++ src/test/java/com/example/simple/naver/NaverClientTest.java
@@ -1,11 +1,11 @@
 package com.example.simple.naver;
 
+import com.example.simple.naver.dto.SearchImageReq;
 import com.example.simple.naver.dto.SearchLocalReq;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class NaverClientTest {
@@ -14,11 +14,19 @@ class NaverClientTest {
     private NaverClient naverClient;
 
     @Test
-    public void searchTest() {
+    public void search(){
         var search = new SearchLocalReq();
         search.setQuery("목포 맛집");
-
         var result = naverClient.searchLocal(search);
         System.out.println(result);
     }
+
+    @Test
+    public void searchImage(){
+        var search = new SearchImageReq();
+        search.setQuery("카페");
+        var result = naverClient.searchImage(search);
+        System.out.println(result);
+    }
+
 }
