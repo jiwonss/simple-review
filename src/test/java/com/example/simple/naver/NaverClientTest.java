96425fd35159fb5f1171bd277fa3bd9570597464
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2022-06-22 23:30:12 +0900

    NaverClientTest

--- src/test/java/com/example/simple/naver/NaverClientTest.java
+++ src/test/java/com/example/simple/naver/NaverClientTest.java
@@ -1,32 +1,44 @@
 package com.example.simple.naver;
 
 import com.example.simple.naver.dto.SearchImageReq;
+import com.example.simple.naver.dto.SearchImageRes;
 import com.example.simple.naver.dto.SearchLocalReq;
+import com.example.simple.naver.dto.SearchLocalRes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
-class NaverClientTest {
+public class NaverClientTest {
 
     @Autowired
     private NaverClient naverClient;
 
     @Test
-    public void search(){
-        var search = new SearchLocalReq();
-        search.setQuery("목포 카페");
-        var result = naverClient.searchLocal(search);
-        System.out.println(result);
+    public void hello() {
+        System.out.println("hello");
     }
 
     @Test
-    public void searchImage(){
-        var search = new SearchImageReq();
-        search.setQuery("씨엘비베이커리");
-        var result = naverClient.searchImage(search);
-        System.out.println(result);
+    public void searchLocal() {
+        String query = "서울 카페";
+
+        SearchLocalReq searchLocalReq = new SearchLocalReq();
+        searchLocalReq.setQuery(query);
+
+        SearchLocalRes searchLocalRes = naverClient.searchLocal(searchLocalReq);
+        System.out.println(searchLocalRes.getItems());
+    }
+
+    @Test
+    public void searchImage() {
+        String query = "서울 카페";
+
+        SearchImageReq searchImageReq = new SearchImageReq();
+        searchImageReq.setQuery(query);
+
+        SearchImageRes searchImageRes = naverClient.searchImage(searchImageReq);
+        System.out.println(searchImageRes.getItems());
     }
 
 }
