eec07ae86369e9144010606c66adb7be11a7628a
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2022-07-07 01:28:48 +0900

    Password 변경 시 현재 Password와 비교 추가

--- src/main/java/com/example/simple/validation/PasswordValidator.java
+++ src/main/java/com/example/simple/validation/PasswordValidator.java
@@ -1,6 +1,10 @@
 package com.example.simple.validation;
 
 import com.example.simple.user.dto.PasswordDto;
+import com.example.simple.user.entity.UserEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -13,6 +17,14 @@ public class PasswordValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         PasswordDto passwordDto = (PasswordDto) target;
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserEntity userEntity = (UserEntity) authentication.getPrincipal();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        if (encoder.matches(passwordDto.getNewPassword(), userEntity.getPassword())) {
+            errors.rejectValue("newPassword", "same value", "현재 패스워드와 일치합니다.");
+        }
 
         if (!passwordDto.getNewPassword().equals(passwordDto.getNewPasswordConfirm())) {
             errors.rejectValue("newPassword", "wrong value", "입력한 새 패스워드가 일치하지 않습니다.");
